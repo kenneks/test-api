277a2d208f224cdf5c877ff34ba6b7b7a93fd8e3
Kenneth Strand <kenstra@live.com>
2017-08-30 23:35:55 +0200

    Lagt til Hello world API-endepunkt

--- src/main/java/com/github/kenneks/testapi/ApplicationRunner.java
+++ src/main/java/com/github/kenneks/testapi/ApplicationRunner.java
@@ -1,8 +1,13 @@
 package com.github.kenneks.testapi;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApplicationRunner {
+	
+	public static void main(String[] args) {
+        SpringApplication.run(ApplicationRunner.class, args);
+    }
 
 }
